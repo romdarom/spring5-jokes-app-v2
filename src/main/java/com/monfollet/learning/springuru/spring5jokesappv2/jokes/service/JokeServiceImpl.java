a57650ff79953efa3872d19c486804abeb34714e
romain <monfollet.romain@hotmail.fr>
2021-09-18 11:59:25 +0200

    DIP for Service

---
+++ src/main/java/com/monfollet/learning/springuru/spring5jokesappv2/jokes/service/JokeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.monfollet.learning.springuru.spring5jokesappv2.jokes.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
+    @Autowired
+    public void setChuckNorrisQuotes(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
+    public String getNextJoke(){
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+
+
+}
